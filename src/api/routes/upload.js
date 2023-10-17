3bb3d359059cd6845eecbfb5eb7df5bb7df45bbe
JH51 <jonhuang5@gmail.com>
2023-10-17 12:34:24 -0400

    Add router uploadRouter

---
+++ src/api/routes/upload.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+
+// const fileController = require('../controllers/fileController');
+// const storageController = require('../controllers/storageController');
+
+const upload = multer({ dest: 'public/data/uploads/' });
+
+const uploadRouter = express.Router();
+
+uploadRouter.get('/', (req, res) => {
+  res.status(200).send(req.originalUrl);
+});
+
+uploadRouter.post(
+  '/',
+  upload.single('user-upload'),
+  // fileController.validateSingleFileType,
+  // storageController.putObject,
+  // fileController.clearStoredUploads,
+  (req, res) => {
+    // file type validation, use env variables
+    res.sendStatus(201);
+  }
+);
+
+module.exports = uploadRouter;
