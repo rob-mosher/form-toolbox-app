cb0dd129fac9b78d2fe09c658b976f1d0a447a3b
Rob Mosher <rob@robmosher.com>
2023-10-24 19:43:55 -0400

    Refactor UploadForm logic

--- src/web/src/pages/UploadForm.jsx
+++ src/web/src/pages/UploadForm.jsx
@@ -4,6 +4,47 @@ import { Button, Header, Input } from 'semantic-ui-react'
 export default function UploadForm() {
   const fileRef = useRef(null)
   const [imageFile, setImageFile] = useState(null)
+
+  function uploadFile() {
+    function checkFilePromise() {
+      const file = fileRef?.current?.files?.[0]
+      if (!file) throw new Error('No file provided.')
+      if (![
+        'image/jpg',
+        'image/jpeg',
+        'image/png',
+        'application/pdf',
+      ].includes(file.type)
+      ) { throw new Error('Invalid file type.') }
+
+      const formData = new FormData()
+      formData.append('user-upload', file)
+      return Promise.resolve(formData)
+    }
+
+    checkFilePromise()
+      .then((formData) => fetch(
+        `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`,
+        {
+          body: formData,
+          method: 'POST',
+        }
+      ))
+      .then((fetchResult) => {
+        if (!fetchResult) throw new Error('Server did not provide a response.')
+        if (fetchResult.ok) return fetchResult.text()
+        throw new Error(`${fetchResult.status} ${fetchResult.statusText}`)
+      })
+      .then((textResult) => {
+        // TODO update web application state
+        console.log(textResult)
+      })
+      .catch((error) => {
+        // error handling
+        console.error(error) // TODO May want to replace with ErrorBoundary
+      })
+  }
+
   return (
     <>
       <Header as='h2'>Upload</Header>
@@ -27,41 +68,7 @@ export default function UploadForm() {
       </Input>
       <br />
       <Button
-        onClick={() => {
-          Promise.resolve()
-            .then(() => {
-              if (!fileRef?.current?.files?.[0]) throw new Error('No file selected')
-              if (
-                !['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'].includes(
-                  fileRef?.current?.files?.[0]?.type || ''
-                )
-              ) { throw new Error('Invalid file type') }
-              const formData = new FormData()
-              formData.append('user-upload', fileRef.current.files[0])
-              return formData
-            })
-            .then((formData) => fetch(
-              `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`,
-              {
-                body: formData,
-                method: 'POST',
-              }
-            ))
-            .then((fetchResult) => {
-              if (!fetchResult) throw new Error('Server did not provide a response')
-              if (!fetchResult.ok) throw new Error(`${fetchResult.status} ${fetchResult.statusText}`)
-              if (fetchResult.status === 201) return fetchResult.text()
-              throw new Error('Unknown error occured')
-            })
-            .then((textResult) => {
-              // TODO update web application state
-              console.log(textResult)
-            })
-            .catch((error) => {
-              // error handling
-              console.error(error) // TODO May want to replace with ErrorBoundary
-            })
-        }}
+        onClick={() => { uploadFile() }}
         primary
       >
         Upload
