b0a1b26d7273346baca7721abcc2151bf1c3c528
Rob Mosher <rob@robmosher.com>
2023-10-24 20:28:32 -0400

    Convert upload to form and stylize a bit
    Commented out Cancel button for now until settled on logic.

--- src/web/src/pages/UploadForm.jsx
+++ src/web/src/pages/UploadForm.jsx
@@ -1,24 +1,18 @@
 import { useRef, useState } from 'react'
-import { Button, Header, Input } from 'semantic-ui-react'
+import {
+  Button, Header, Input, Segment
+} from 'semantic-ui-react'
 import ACCEPTED_UPLOAD_MIME_TYPES from '../common/acceptedUploadMimeTypes'
 
 export default function UploadForm() {
   const fileRef = useRef(null)
   const [imageFile, setImageFile] = useState(null)
 
-  function uploadFile() {
-    function checkFilePromise() {
-      const file = fileRef?.current?.files?.[0]
-      if (!file) throw new Error('No file provided.')
-      if (!ACCEPTED_UPLOAD_MIME_TYPES.includes(file.type)
-      ) { throw new Error('Invalid file type.') }
+  function handleUpload(e) {
+    e.preventDefault()
 
-      const formData = new FormData()
-      formData.append('user-upload', file)
-      return Promise.resolve(formData)
-    }
-
-    checkFilePromise()
+    // eslint-disable-next-line no-use-before-define
+    validateFilePromise()
       .then((formData) => fetch(
         `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`,
         {
@@ -39,43 +33,56 @@ export default function UploadForm() {
         // error handling
         console.error(error) // TODO May want to replace with ErrorBoundary
       })
+
+    function validateFilePromise() {
+      const file = fileRef?.current?.files?.[0]
+      if (!file) throw new Error('No file provided.')
+      if (!ACCEPTED_UPLOAD_MIME_TYPES.includes(file.type)
+      ) { throw new Error('Invalid file type.') }
+
+      const formData = new FormData()
+      formData.append('user-upload', file)
+      return Promise.resolve(formData)
+    }
   }
 
   return (
-    <>
-      <Header as='h2'>Upload</Header>
-      <Input type='file'>
-        <input
-          accept={ACCEPTED_UPLOAD_MIME_TYPES.join(', ')}
-          id='uploadpicker'
+    <form onSubmit={(e) => handleUpload(e)}>
+      <Segment placeholder>
+        <Header as='h2' icon>Upload</Header>
+        <Input type='file'>
+          <input
+            accept={ACCEPTED_UPLOAD_MIME_TYPES.join(', ')}
+            id='uploadpicker'
           // multiple
-          name='user-upload'
-          onInput={(event) => {
-            event.preventDefault()
-            if (fileRef?.current?.files?.[0]?.type?.includes('image')) {
-              setImageFile(() => fileRef?.current?.files?.[0])
-            } else {
-              setImageFile(() => null)
-            }
-          }}
-          ref={fileRef}
-          type='file'
-        />
-      </Input>
-      <br />
-      <Button
-        onClick={() => { uploadFile() }}
-        primary
-      >
-        Upload
-      </Button>
-      <Button secondary>Cancel</Button>
-      {imageFile?.type?.includes('image') && (
-        <>
-          <br />
-          <img src={URL.createObjectURL(imageFile)} alt='' />
-        </>
-      )}
-    </>
+            name='user-upload'
+            onInput={(event) => {
+              event.preventDefault()
+              if (fileRef?.current?.files?.[0]?.type?.includes('image')) {
+                setImageFile(() => fileRef?.current?.files?.[0])
+              } else {
+                setImageFile(() => null)
+              }
+            }}
+            ref={fileRef}
+            type='file'
+          />
+        </Input>
+        <br />
+        <Button
+          type='submit'
+          primary
+        >
+          Upload
+        </Button>
+        {/* <Button secondary>Cancel</Button> */}
+        {imageFile?.type?.includes('image') && (
+          <>
+            <br />
+            <img src={URL.createObjectURL(imageFile)} alt='' />
+          </>
+        )}
+      </Segment>
+    </form>
   )
 }
