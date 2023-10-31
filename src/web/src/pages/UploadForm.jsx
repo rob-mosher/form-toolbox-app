965e14d56b8e6cb941c0832bed879cf011a3ac38
Rob Mosher <rob@robmosher.com>
2023-10-31 17:28:27 -0400

    Add upload state logic
    Commented out image preview for now.

--- src/web/src/pages/UploadForm.jsx
+++ src/web/src/pages/UploadForm.jsx
@@ -2,47 +2,64 @@ import { useRef, useState } from 'react'
 import {
   Button, Header, Input, Segment
 } from 'semantic-ui-react'
+import { toast } from 'react-toastify'
 import ACCEPTED_UPLOAD_MIME_TYPES from '../common/acceptedUploadMimeTypes'
 
 export default function UploadForm() {
   const fileRef = useRef(null)
-  const [imageFile, setImageFile] = useState(null)
+  const [isUploading, setIsUploading] = useState(false)
+  // const [imageFile, setImageFile] = useState(null)
 
   function handleUpload(e) {
     e.preventDefault()
 
+    setIsUploading(true)
+    const filePointer = fileRef?.current?.files?.[0]
+
     // eslint-disable-next-line no-use-before-define
-    validateFilePromise()
-      .then((formData) => fetch(
-        `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`,
-        {
-          body: formData,
-          method: 'POST',
-        }
-      ))
+    validateFilePromise(filePointer)
+      .then((formData) => {
+        toast.info('Uploading file.')
+
+        const url = `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`
+        return fetch(
+          url,
+          {
+            body: formData,
+            method: 'POST',
+          }
+        )
+      })
       .then((fetchResult) => {
         if (!fetchResult) throw new Error('Server did not provide a response.')
         if (fetchResult.ok) return fetchResult.text()
         throw new Error(`${fetchResult.status} ${fetchResult.statusText}`)
       })
-      .then((textResult) => {
-        // TODO update web application state
-        console.log(textResult)
+      .then(() => {
+        fileRef.current.value = null
+        toast.success('Upload complete!')
       })
       .catch((error) => {
-        // error handling
-        console.error(error) // TODO May want to replace with ErrorBoundary
+        toast.error(`Upload unsuccessful: ${error.message}`)
+      })
+      .finally(() => {
+        setIsUploading(false)
       })
 
-    function validateFilePromise() {
-      const file = fileRef?.current?.files?.[0]
-      if (!file) throw new Error('No file provided.')
-      if (!ACCEPTED_UPLOAD_MIME_TYPES.includes(file.type)
-      ) { throw new Error('Invalid file type.') }
+    function validateFilePromise(file) {
+      return new Promise((resolve, reject) => {
+        if (!file) {
+          reject(new Error('No file provided.'))
+        }
 
-      const formData = new FormData()
-      formData.append('user-upload', file)
-      return Promise.resolve(formData)
+        if (!ACCEPTED_UPLOAD_MIME_TYPES.includes(file.type)) {
+          reject(new Error('Invalid file type.'))
+        }
+
+        const formData = new FormData()
+        formData.append('user-upload', file)
+        resolve(formData)
+      })
     }
   }
 
@@ -53,35 +70,37 @@ export default function UploadForm() {
         <Input type='file'>
           <input
             accept={ACCEPTED_UPLOAD_MIME_TYPES.join(', ')}
-            id='uploadpicker'
-          // multiple
+            id='upload-picker'
+            // multiple
             name='user-upload'
-            onInput={(event) => {
-              event.preventDefault()
-              if (fileRef?.current?.files?.[0]?.type?.includes('image')) {
-                setImageFile(() => fileRef?.current?.files?.[0])
-              } else {
-                setImageFile(() => null)
-              }
-            }}
+            // onInput={(event) => {
+            //   event.preventDefault()
+            //   if (fileRef?.current?.files?.[0]?.type?.includes('image')) {
+            //     setImageFile(() => fileRef?.current?.files?.[0])
+            //   } else {
+            //     setImageFile(() => null)
+            //   }
+            // }}
             ref={fileRef}
             type='file'
           />
         </Input>
         <br />
         <Button
+          className={isUploading ? 'disabled' : ''}
+          id='upload-button'
           type='submit'
           primary
         >
           Upload
         </Button>
         {/* <Button secondary>Cancel</Button> */}
-        {imageFile?.type?.includes('image') && (
+        {/* {imageFile?.type?.includes('image') && (
           <>
             <br />
             <img src={URL.createObjectURL(imageFile)} alt='' />
           </>
-        )}
+        )} */}
       </Segment>
     </form>
   )
