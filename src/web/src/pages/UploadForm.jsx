510c65c5d4ceb49a64aba0e624b962ff8781dd3a
Rob Mosher <rob@robmosher.com>
2023-11-01 14:16:53 -0400

    Add progress bar to upload
    BUG: If user navigates away from, and back to, the upload page during
         an upload, state is not maintained correctly
    BUG: 'toast' is not consistently closing after a completed upload

--- src/web/src/pages/UploadForm.jsx
+++ src/web/src/pages/UploadForm.jsx
@@ -3,10 +3,12 @@ import {
   Button, Header, Input, Segment
 } from 'semantic-ui-react'
 import { toast } from 'react-toastify'
+import axios from 'axios'
 import ACCEPTED_UPLOAD_MIME_TYPES from '../common/acceptedUploadMimeTypes'
 
 export default function UploadForm() {
   const fileRef = useRef(null)
+  const toastRef = useRef(null)
   const [isUploading, setIsUploading] = useState(false)
   // const [imageFile, setImageFile] = useState(null)
 
@@ -14,33 +16,45 @@ export default function UploadForm() {
     e.preventDefault()
 
     setIsUploading(true)
+    toastRef.current = toast('Upload in progress...', {
+      // autoClose: false,
+      progress: 0,
+      type: toast.TYPE.INFO,
+    })
+
     const filePointer = fileRef?.current?.files?.[0]
 
     // eslint-disable-next-line no-use-before-define
     validateFilePromise(filePointer)
       .then((formData) => {
-        toast.info('Uploading file.')
-
         const url = `//${import.meta.env.VITE_API_HOST || '127.0.0.1'}:${import.meta.env.VITE_API_PORT || 3000}/api/upload`
-        return fetch(
-          url,
+
+        return axios.request(
           {
-            body: formData,
+            data: formData,
             method: 'POST',
+            onUploadProgress: (prog) => {
+              const progress = prog.loaded / prog.total
+              toast.update(toastRef.current, { progress })
+            },
+            url,
           }
         )
       })
-      .then((fetchResult) => {
-        if (!fetchResult) throw new Error('Server did not provide a response.')
-        if (fetchResult.ok) return fetchResult.text()
-        throw new Error(`${fetchResult.status} ${fetchResult.statusText}`)
-      })
       .then(() => {
+        toast.update(toastRef.current, {
+          autoClose: 5000,
+          render: 'Upload complete!',
+          type: toast.TYPE.SUCCESS,
+        })
         fileRef.current.value = null
-        toast.success('Upload complete!')
       })
       .catch((error) => {
-        toast.error(`Upload unsuccessful: ${error.message}`)
+        toast.update(toastRef.current, {
+          autoClose: 5000,
+          render: `Upload unsuccessful: ${error.message}`,
+          type: toast.TYPE.ERROR,
+        })
       })
       .finally(() => {
         setIsUploading(false)
@@ -70,6 +84,7 @@ export default function UploadForm() {
         <Input type='file'>
           <input
             accept={ACCEPTED_UPLOAD_MIME_TYPES.join(', ')}
+            disabled={isUploading}
             id='upload-picker'
             // multiple
             name='user-upload'
