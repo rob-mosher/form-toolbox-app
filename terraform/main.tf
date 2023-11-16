56867bd1589bf1c65f6a89a7771ac752d6626b96
Rob Mosher <rob@robmosher.com>
2023-11-16 14:21:12 -0500

    Hotfix: set sqs queue name dynamically

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,7 +158,7 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 }
 
 resource "aws_sqs_queue" "form_toolbox_queue" {
-  name                       = "formToolboxQueue"
+  name                       = var.queue_name
   visibility_timeout_seconds = 30
   message_retention_seconds  = 345600
   max_message_size           = 262144
