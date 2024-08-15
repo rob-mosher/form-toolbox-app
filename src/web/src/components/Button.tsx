bd3b91d41b5087611ddda83d074582b97575e3d1
Rob Mosher <rob@robmosher.com>
2024-08-15 06:48:37 -0400

    Add ariaLabel and onClick to Button component

--- src/web/src/components/Button.tsx
+++ src/web/src/components/Button.tsx
@@ -1,35 +1,39 @@
 import clsx from 'clsx'
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 interface ButtonProps {
+  ariaLabel: string;
   children?: ReactNode;
   disabled?: boolean;
   id: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => unknown;
   primary?: boolean;
   type?: 'button' | 'submit';
 }
 
 export default function Button({
+  ariaLabel,
   children = 'Button',
   disabled = false,
   id,
   primary = false,
+  onClick,
   type = 'button',
 }: ButtonProps) {
   return (
     <button
-      id={id}
-      disabled={disabled}
+      aria-label={ariaLabel}
       className={clsx(
-        'rounded-lg px-7 py-3 font-semibold',
-        primary ? 'bg-sky-600 text-white' : 'bg-stone-300 text-stone-700',
-        disabled && 'opacity-60'
+        'rounded-lg px-7 py-3 font-semibold disabled:opacity-60',
+        primary ? 'bg-sky-600 text-white' : 'bg-stone-300 text-stone-700'
       )}
+      disabled={disabled}
+      id={id}
+      onClick={onClick}
       // TODO resolve typescript issue
       // eslint-disable-next-line react/button-has-type
       type={type}
     >
-      {disabled}
       {children}
     </button>
   )
