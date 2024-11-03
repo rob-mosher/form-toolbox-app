eb495f228593a2bea66a1a4ad5b64704ad6b04ad
Rob Mosher <rob@robmosher.com>
2024-11-02 21:51:53 -0400

    refactor(terraform): relocate lambda functions into terraform/lambda directory
    Reduces clutter in the project root and centralizes infrastructure logic.

--- terraform/lambda/data_sources.tf
+++ terraform/lambda/data_sources.tf
@@ -1,5 +1,5 @@
 data "archive_file" "on_upload_lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../functions/onUpload"
+  source_dir  = "${path.module}/functions/onUpload"
   output_path = "${path.module}/artifacts/on_upload_lambda_function.zip"
 }
